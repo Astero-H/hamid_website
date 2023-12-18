eea5f791f832c315f0174be1db3b20156abb1bee
Astero-H <ahhennane@gmail.com>
2023-12-19 00:37:59 +0100

    Factorizing code for languages

--- index.php
+++ index.php
@@ -3,21 +3,12 @@ use Dotenv\Dotenv;
 require_once 'vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
-switch ($lang) {
-    case 'fr': 
-        include $_ENV['LANG_PATH']. '/txt_fr.php';
-        break;
-    case 'en': 
-        include $_ENV['LANG_PATH']. '/txt_en.php';
-        break;
-    case 'es': 
-        include $_ENV['LANG_PATH']. '/txt_es.php';
-        break;
-    default: 
-        include $_ENV['LANG_PATH']. '/txt_en.php';
-        break;
-}
+// Loading accepted languages
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+$acceptLang = ['fr', 'es', 'en']; 
+$lang = in_array($lang, $acceptLang) ? $lang : 'en';
+include_once $_ENV['LANG_PATH'] . "/txt_{$lang}.php"; 
+
 require 'template-cv.php';
 ?>
