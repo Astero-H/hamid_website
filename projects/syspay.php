b7d12791a22c086999637febdda6a4388485b48f
Astero-H <ahhennane@gmail.com>
2023-12-22 03:41:46 +0100

    Formating projects files

--- projects/syspay.php
+++ projects/syspay.php
@@ -1,76 +1,83 @@
-     <div class="section section-twoside fp-auto-height-responsive section-item" data-section="syspay">
-      <div class="section-cover-tier mask-black" style="background-image:url('../public/img/items/syspay_cover.jpg')">
+<div class="section section-twoside fp-auto-height-responsive section-item" data-section="syspay">
+  <div class="section-cover-tier mask-black" style="background-image:url('../public/img/items/syspay_cover.jpg')">
+  </div>
+  <div class="section-wrapper largewidth">
+    <div class="item row">
+      <div class="col-12 col-md-4">
       </div>
-      <div class="section-wrapper largewidth">
-        <div class="item row">
-          <div class="col-12 col-md-4">
-          </div>
-          <div class="col-12 col-md-8 center-vh">
-            <!-- content -->
-            <div class="section-content anim scrollable">
-              <article class="article article-light">
-                <header class="article-header">
-                  <div class="wrapper">
-                    <div class="media media-article-title">
+      <div class="col-12 col-md-8 center-vh">
+        <!-- content -->
+        <div class="section-content anim scrollable">
+          <article class="article article-light">
+            <header class="article-header">
+              <div class="wrapper">
+                <div class="media media-article-title">
 
-                      <div class="media-body align-self-center">
-                        <h5 class="mt-0 media-title">
-                          <strong>SYSPAY - PHP Fullstack Developer</strong>
-                        </h5>
-                        <p class="mb-0 t-nowrap"></p>
-                        <p class="mb-0 t-nowrap">November 2022 - October 2023 </p>
-                      </div>
-                    </div>
-                  </div>
-                </header>
-                <div class="article-content">
-                  <div class="wrapper">
-                    <h2>Syspay - Payment System</h2>
-                    <h2></h2>
-                    <ul>
-                      <li> Maintenance de l’application Syspay et du moteur de transactions</li>
-                      <li> Reverse engineering et ajout de documentation (Syspay API)</li>
-                      <li> Création de nouvelles features et bugfix (Redmine)</li>
-                    </ul>
-                    <p class="text-syspay">
-                      En qualité de développeur fullstack PHP, je travaillais en collaboration étroite avec d'autres développeurs sur le système de paiement proposé par Syspay. Syspay propose 
-                      des services de paiement utilisés par des boutiques e-commerce, éditeurs de logiciels et plateformes numériques.<br><br>
-
-                      Mon travail consistait à trouver des solutions aux problèmes rencontrés sur l'application Syspay par des collaborateurs ou membres de la société. Je devais également
-                      mettre en place de nouvelles fonctionnalités et renseigner la documentation relative à l'application. Entre-autres je devais apporter une analyse du code métier
-                      et rétro-documenter le fonctionnement de modules ou parties complexes de l'application.<br>
-
-                      Syspay propose une API sur laquelle j'ai eu l'occasion de travailler à travers des tests de paiements spécifiques, des modifications de méthodes de paiements ou de mises 
-                      de solutions d'API tierces (service d'encaissement), j'ai pu travailler sur le moteur de transaction Syspay.<br><br>
-                      
-                      Cette expérience m'a permis d'en apprendre beaucoup sur les fournisseurs de paiements, les intéractions entre les différents acteurs d'une transaction mais également 
-                      les règles indispensables de sécurité qui découlent de ces opérations.<br>
-                    </p>
+                  <div class="media-body align-self-center">
+                    <h5 class="mt-0 media-title">
+                      <strong>SYSPAY - PHP Fullstack Developer</strong>
+                    </h5>
+                    <p class="mb-0 t-nowrap"></p>
+                    <p class="mb-0 t-nowrap">November 2022 - October 2023 </p>
                   </div>
                 </div>
-                <footer class="article-footer">
-                  <div class="article-details">
-                    <div class="media media-article-title">
-                      <div class="media-body align-self-center">
-                        <h5 class="mt-0 media-title">
-                          <strong>Technologies</strong>
-                        </h5>
-                        <p class="mb-0 t-nowrap"> Symfony 4 - PHP 7 - JQuery - Doctrine - Docker - Postman - Kibana - Git - Bash - REST </p>
-                      </div>
-                    </div>
-                  </div>
-                </footer>
-              </article>
-              <!-- Action button -->
-              <div class="btns-action mt-4">
-                <a class="btn btn-transp-arrow btn-outline btn-primary btn-round" href="project_template.php?p=cgi">
-                  <span class="icon arrow-right"></span>
-                  <span class="text">Next</span>
-                </a>
+              </div>
+            </header>
+            <div class="article-content">
+              <div class="wrapper">
+                <h2>Syspay - Payment System</h2>
+                <ul>
+                  <li> Maintenance de l’application Syspay et du moteur de transactions</li>
+                  <li> Reverse engineering et ajout de documentation (Syspay API)</li>
+                  <li> Création de nouvelles features et bugfix (Redmine)</li>
+                </ul>
+                <p class="text-syspay">
+                  En qualité de développeur fullstack PHP, je travaillais en collaboration étroite avec d'autres
+                  développeurs sur le système de paiement proposé par Syspay. Syspay propose
+                  des services de paiement utilisés par des boutiques e-commerce, éditeurs de logiciels et plateformes
+                  numériques.<br><br>
+
+                  Mon travail consistait à trouver des solutions aux problèmes rencontrés sur l'application Syspay par
+                  des collaborateurs ou membres de la société. Je devais également
+                  mettre en place de nouvelles fonctionnalités et renseigner la documentation relative à l'application.
+                  Entre-autres je devais apporter une analyse du code métier
+                  et rétro-documenter le fonctionnement de modules ou parties complexes de l'application.<br>
+
+                  Syspay propose une API sur laquelle j'ai eu l'occasion de travailler à travers des tests de paiements
+                  spécifiques, des modifications de méthodes de paiements ou de mises
+                  de solutions d'API tierces (service d'encaissement), j'ai pu travailler sur le moteur de transaction
+                  Syspay.<br><br>
+
+                  Cette expérience m'a permis d'en apprendre beaucoup sur les fournisseurs de paiements, les
+                  intéractions entre les différents acteurs d'une transaction mais également
+                  les règles indispensables de sécurité qui découlent de ces opérations.<br>
+                </p>
               </div>
             </div>
+            <footer class="article-footer">
+              <div class="article-details">
+                <div class="media media-article-title">
+                  <div class="media-body align-self-center">
+                    <h5 class="mt-0 media-title">
+                      <strong>Technologies</strong>
+                    </h5>
+                    <p class="mb-0 t-nowrap"> Symfony 4 - PHP 7 - JQuery - Doctrine - Docker - Postman - Kibana - Git -
+                      Bash - REST </p>
+                  </div>
+                </div>
+              </div>
+            </footer>
+          </article>
+          <!-- Action button -->
+          <div class="btns-action mt-4">
+            <a class="btn btn-transp-arrow btn-outline btn-primary btn-round" href="project_template.php?p=cgi">
+              <span class="icon arrow-right"></span>
+              <span class="text">Next</span>
+            </a>
           </div>
-          <!-- end of text content -->
         </div>
       </div>
+      <!-- end of text content -->
     </div>
+  </div>
+</div>
